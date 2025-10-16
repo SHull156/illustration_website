7c4984f186880d20c08a90ca515476032ba0175e
SHull156 <sarahthull@gmail.com>
2025-10-16 19:26:49 +0100

    Updating to railway's port

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,9 @@ app.get("/products", (req, res) => {
   });
 });
 
-app.listen(4000, () => console.log("Server running on http://localhost:4000"));
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
